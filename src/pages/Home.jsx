7dd10babf63f1eb3bbd3f53e2a44c43fc4bc08fa
Sanki0 <sebastian.ferreyra.c@uni.pe>
2021-11-27 19:17:12 -0500

    first style for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Header } from "../components/Header";
+import "../styles/Home.scss"
 
 
 function Home() {
   return (
     <>
       <Header />
-      <body>
+      <div className="home">
+        <h1 className="title-home">CRC cards Generator</h1>
+        <div className="descripcion">
+          aqui va una descripcion del proyecto
+        </div>
         <div className="mision">
+          <h2>Mision</h2>
           aqui va la mision
         </div>
         <div className="vision">
+          <h2>Vision</h2>
           aqui va la vision
         </div>
-      </body>
+      </div>
     </>
   )
 }
